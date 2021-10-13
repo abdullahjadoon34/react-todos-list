5e8bae24cc8458f0e747fdf63b481ced11d7421b
abdullahjadoon34 <abdullahjadoon75@gmail.com>
2021-10-13 16:42:58 +0500

    changes in backend

--- backend/src/database.js
+++ backend/src/database.js
@@ -3,5 +3,5 @@ const { MongoClient } = require('mongodb');
 const database = module.exports;
 
 database.connect = async function connect() {
-  database.client = await MongoClient.connect('mongodb://root:1234@database/', { useUnifiedTopology: true });
+  database.client = await MongoClient.connect('mongodb://localhost:27017/db1', { useUnifiedTopology: true });
 };
