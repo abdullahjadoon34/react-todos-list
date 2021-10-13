7c827481b1dcd7a60d6e9070b9d90331f50ecc5c
abdullahjadoon34 <abdullahjadoon75@gmail.com>
2021-10-13 23:58:02 +0500

    heroku first commit

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 const database = module.exports;
-
+//'mongodb://root:1234@database/
 database.connect = async function connect() {
-  database.client = await MongoClient.connect('mongodb://localhost:27017/db1', { useUnifiedTopology: true });
+  database.client = await MongoClient.connect('mongodb://localhost:27017/database', { useUnifiedTopology: true });
 };
